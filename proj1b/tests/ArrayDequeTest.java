2da2e9d8216a419136e233e30eaa811c494aaeea
Yuxin Li <ylinq@connect.ust.hk>
2024-07-11 21:17:42 -0400

    Delete proj1b directory

--- proj1b/tests/ArrayDequeTest.java
+++
@@ -1,8 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-
-public class ArrayDequeTest {
-
-}
